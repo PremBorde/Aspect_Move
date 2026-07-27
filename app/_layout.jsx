789e72392c9940f0d3629b7d63d67449ffe69415
Prem Borde <prembordesn4@gmail.com>
2026-07-27 14:35:25 +0530

    chore: successful APK build with official Aspect Move logo

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,4 +1,3 @@
-import "../global.css";
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { AuthProvider } from "../context/AuthContext";
